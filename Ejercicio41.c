42f8f5bda875b3240ec8f9491e6f6eb314c6c60d
Roberto <robertoulloau@gmail.com>
2023-04-16 18:29:51 -0500

    Ejercicio41.c

--- Ejercicio41.c
+++ Ejercicio41.c
@@ -2,7 +2,18 @@
 
 int main()
 {
-    printf("hola");
+    /*Serie Fibonacci entre el 0 y 100*/
+    int n1=0, n2=1, n3;
+    printf("%d, %d, ",n1,n2);
+    
+    do
+    {
+        n3=n1+n2;
+        printf("%d, ",n3);
+        n1=n2;
+        n2=n3;
+        
+    }while (n2<89);
 
     return 0;
 }

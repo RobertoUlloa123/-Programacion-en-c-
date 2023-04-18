7fe2127626611e60925ab6ad4d9fa58590b7ad7a
Roberto <robertoulloau@gmail.com>
2023-04-17 20:40:30 -0500

    Programacion en c

---
+++ Ejercicio14.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int n=3,resultado;
+     printf("Los primeros 20 multiplos de 3 son: ");
+   
+    for(int cont=1; cont<=20; cont++) /*Hacemos que el contador se multiplique por 3 hasta llegar a 20 y despues imprima el resultado*/
+    {
+        resultado=n*cont;
+       printf("%d, ",resultado);
+    }
+
+    return 0;
+}
